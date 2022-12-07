de27422bc4d36d83b225145e0f320b43b9842360
Arshia Nazarlotfi <arshia_nazarlotfi1376@yahoo.com>
2022-12-07 13:35:47 +0330

    removing redux from app

--- pages/_app.js
+++ pages/_app.js
@@ -8,34 +8,30 @@ import 'react-toastify/dist/ReactToastify.css';
 import Layout from '../components/common/Layout';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { SessionProvider } from 'next-auth/react';
-import { Provider } from 'react-redux';
-import { store } from '../store';
 import { ToastContainer } from 'react-toastify';
 
 function MyApp({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <Provider store={store}>
-      <SessionProvider session={pageProps.session}>
-        <QueryClientProvider client={queryClient}>
-          <Layout>
-            <Component {...pageProps} />
-            <ToastContainer
-              position='top-right'
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick
-              rtl={true}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              style={{ fontSize: '14px' }}
-            />
-          </Layout>
-        </QueryClientProvider>
-      </SessionProvider>
-    </Provider>
+    <SessionProvider session={pageProps.session}>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+          <ToastContainer
+            position='top-right'
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={true}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            style={{ fontSize: '14px' }}
+          />
+        </Layout>
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
 
--- store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import postSlice from './features/postSlice';
-
-export const store = configureStore({
-  reducer: {
-    post: postSlice,
-  },
-});
