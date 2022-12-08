23a4c95d4ff9f763bdc39803af1058721f2fc053
Arshia Nazarlotfi <arshia_nazarlotfi1376@yahoo.com>
2022-12-08 15:48:08 +0330

    adding new domain

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['www.clinicdermatech.com', 'wallpapercave.com', 'localhost'],
+    domains: [
+      'www.clinicdermatech.com',
+      'wallpapercave.com',
+      'localhost',
+      'https://next-blog-api.cyclic.app',
+    ],
   },
 };
 
