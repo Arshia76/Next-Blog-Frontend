a6864625aee4d7803b59878b04a1ca6fc2b18990
Arshia Nazarlotfi <arshia_nazarlotfi1376@yahoo.com>
2023-01-24 12:35:07 +0330

    reverting to replaceAll

--- pages/posts/[id]/[index].js
+++ pages/posts/[id]/[index].js
@@ -85,7 +85,10 @@ const PostDetailPage = (props) => {
         <div className={styles.Section}>
           <div className={styles.AuthorImage}>
             <Image
-              src={process.env.NEXT_PUBLIC_URL + postData?.creator?.avatar}
+              src={
+                process.env.NEXT_PUBLIC_URL +
+                postData?.creator?.avatar.replaceAll('\\', '/')
+              }
               alt={postData?.creator?.username}
               width={50}
               height={50}
@@ -104,7 +107,10 @@ const PostDetailPage = (props) => {
         </div>
         <div className={styles.Image}>
           <Image
-            src={process.env.NEXT_PUBLIC_URL + postData?.image}
+            src={
+              process.env.NEXT_PUBLIC_URL +
+              postData?.image.replaceAll('\\', '/')
+            }
             alt={postData?.title}
             layout='fill'
             objectFit='cover'
